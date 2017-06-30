6c378f10c0885bbba12123c8e15e3b7968e324fa
Artemtar <artemtarasov89@gmail.com>
2017-06-30 21:43:59 +0900

    adding afull implementation for Search Table

---
+++ algorithms/ST/ST_ARRAY.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "STitem.h"
+static STitem *st;
+static int N;
+
+void STinit(int maxN)
+{
+    st = malloc((maxN) * sizeof(STitem));
+    N = 0;
+}
+
+int STcount(void) { return N; }
+    in the symbol table void
+    STshow(void)
+{
+    int i;
+    for (i = 0; i < N; i++)
+        ITEMshow(st[i]);
+}
+
+void STinsert(STitem x){
+int j = N++;
+Key v = key(x);
+while (++cnt1 && j>0 && less(v, key(st[j-1]))){
+st[j] = st[j-1]; j--;
+}
+st[j] = x;
+}
+
+STitem STsearch(Key v){
+int j;
+for (j = 0; j < N; j++){
+if(++cnt2 && eq(v,key(st[j]))) return st[j];
+if(++cnt2 && less(v, key(st[j]))) break;
+}
+return NULLitem;
+}
