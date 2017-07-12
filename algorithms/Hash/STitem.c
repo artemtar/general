7f38b14c2413cf8443063fd9fbea0cff26f15789
Artemtar <artemtarasov89@gmail.com>
2017-07-12 18:35:25 +0900

    Hash main

---
+++ algorithms/Hash/STitem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "STitem.h"
+#include "ST.h"
+STitem NULLitem = {-1, "NULL"};
+
+Key ITEMrand(void) 
+         { return rand()%1000; }
+int ITEMscan(Key *x) 
+         { return scanf("%d", x); } 
+void ITEMshow(STitem item) 
+         { printf("key = %d, info = ", key(item));
+           puts(info(item));
+         }
---
+++ algorithms/Hash/STitem.h
@@ -0,0 +1,13 @@
+typedef int Key;
+typedef struct { Key key; char info[10]; } STitem;
+#define key(A) (A.key)
+#define info(A) (A.info)
+
+#define less(A, B) (A < B)
+#define eq(A, B) (A == B)
+
+STitem NULLitem;
+
+Key  ITEMrand(void);
+int  ITEMscan(Key *);
+void ITEMshow(STitem);
