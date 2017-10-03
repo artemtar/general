62aca7c5410a4b3c051b8335132926502bc81de9
Artemtar <artemtarasov89@gmail.com>
2017-10-02 20:45:11 -0400

    start screen

---
+++ C++/screen/screen.cpp
@@ -0,0 +1,15 @@
+#include "screen.h"
+
+screen::screen(int w, int h){
+
+}
+screen::clearScreen(char **a){
+    for (int i = 0; i < a.length; i++)
+        a[i] = ' ';
+}
+screen::printScreen(int **a){
+
+}
+screen::gridScreen(int **a, int x, int y){
+
+}
