5212aa0b11c8bde1c72da9cec05bd0ed29f4161e
Artemtar <artemtarasov89@gmail.com>
2017-10-25 07:18:09 -0400

    quinto started

---
+++ C++/Qwinto/scoresheet.cpp
@@ -0,0 +1,101 @@
+#include "scoresheet.h"
+#include <ctime>
+#include <time.h>
+ScoreSheet::ScoreSheet(string name, string lastName) : name_player(name + " " + lastName)
+{
+}
+void ScoreSheet::print(const ostream &os) const
+{
+    //os << name_player << "scored " << score_overall << "points: ";
+}
+void ScoreSheet::score(vector<Dice> d)
+{
+    //int score
+}
+
+//Dice
+void Dice::roll()
+{
+    srand(time(NULL));
+    dice = (rand() % 6) + 1;
+}
+void Dice::print()
+{
+
+    cout << "The first Dice rolled a "
+         << "." << endl;
+
+    cout << "The second Dice rolled a "
+         << "." << endl;
+
+    cout << "Adding both dices up you rolled a total of: " << endl;
+}
+
+//Person
+Person::Person(const string &_firstName, const string &_lastName)
+    : d_firstName(_firstName), d_lastName(_lastName), d_sin(0)
+{
+    d_sin = rand();
+    cout << "new Person" << endl;
+}
+istream &operator>>(istream &_is, Person &_p)
+{
+    _is >> _p.d_firstName >> _p.d_lastName >> _p.d_sin;
+    // check for failure
+    if (!_is)
+        _p = Person();
+    return _is;
+}
+ostream &operator<<(ostream &_os, const Person &_p)
+{
+    _os << _p.d_firstName << "\t" << _p.d_lastName << "\t";
+    _os << _p.d_sin;
+    return _os;
+}
+int main()
+{
+    fstream ioFile;
+    Person personA, personB;
+    // read personA from console
+    cin >> personA;
+    //cout << personA;
+    cout << "-------------------" << endl;
+    // output personA to file
+    ioFile.open("people.txt", ios::out);
+    ioFile << personA;
+    ioFile.close();
+    // read personB from file
+    ioFile.open("people.txt", ios::in);
+    ioFile >> personB;
+    ioFile.close();
+    // output personB to console
+    cout << "Person: " << personB << endl;
+
+    string board[12][34] = {};
+
+    for (int i = 0; i < 12; i +=3)
+    {
+        // cout << endl;
+        
+        // string s(3*(4-i), ' ');
+        // cout << s;
+        for (int j = 0; j < 34; ++j)
+        {
+            board[i][j] = '_';
+        }
+    }
+    cout << endl;
+    for (int k = 0; k < 14; ++k)
+    {
+        cout << endl;
+        if (k == 2) cout << "Red";
+        if (k == 5) cout << "Yellow";
+        if (k == 8) cout << "Blue";
+        for (int l = 0; l < 32; ++l)
+        {
+            cout << board[k][l];
+        }
+    }
+
+    return 0;
+}
---
+++ C++/Qwinto/scoresheet.h
@@ -0,0 +1,72 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+
+class Person;
+// rand() requires
+
+using namespace std;
+using std::string;
+using std::ostream;
+using std::vector;
+using std::cout;
+using std::istream;
+using std::endl;
+using std::fstream;
+using std::cin;
+using std::ios;
+
+static const int g_height = 3;
+static const int g_width = 14;
+struct Dice
+{
+    int dice;
+    int faceValue;
+    void roll();
+    void print();
+};
+class ScoreSheet
+{
+    enum Colour
+    {
+        RED,
+        YELLOW,
+        BLUE
+    };
+    //The class ScoreSheet should hold the scores for the 3 colours, the name of
+    // the player, the number of failed attempts and the overall score.
+    int score_red[16] , score_yellow[16], score_blue[16] = {};
+    int score_3colours[g_height][g_width];
+    int score_overall;
+    int num_failed[];
+    string name_player;
+    //Give the class ScoreSheet a print function that accepts an std::ostream and
+    // inserts the score sheet formatted as in the above example into the stream.
+  public:
+    ScoreSheet(string name = "", string lastName = "");
+    void print(const ostream &os) const;
+    void score(vector<Dice> d);
+};
+
+
+istream &operator>>(istream &, Person &);
+ostream &operator<<(ostream &, const Person &);
+
+class Person
+{
+    string d_lastName;
+    string d_firstName;
+    int d_sin;
+
+  public:
+    Person(const string &_firstName = "",
+           const string &_lastName = "");
+    ~Person()
+    {
+        cout << "~Person: " << d_lastName << endl;
+    }
+    friend istream &operator>>(istream &, Person &);
+    friend ostream &operator<<(ostream &, const Person &);
+};
