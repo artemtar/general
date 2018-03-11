2937811e3b6eeab74f863ee73c83424d4ff7af66
artemtar <artemtarasov89@gmail.com>
2018-03-10 21:19:46 -0500

    traversing prolog

---
+++ PYTHON/usefull/replace_in_file.py
@@ -0,0 +1,9 @@
+with open('file.txt', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('`', '\'')
+
+# Write the file out again
+with open('file.txt', 'w') as file:
+  file.write(filedata)
