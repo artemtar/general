9cac47d3e0d50942795136fc69f69ede772db614
artemtar <artemtarasov89@gmail.com>
2018-10-18 23:46:42 -0400

    uodate

--- other/OCaml/a3.ml
+++ other/OCaml/a3.ml
@@ -237,17 +237,21 @@ let test1b (f:form') : bool = eval_prop f e
    that there are many more efficient ways to implement this data
    structure, which we ignore here.) *)
 
-(* 
+
 module type DICT =
   sig
     type ('key,'value) dict
-    TODO: Insert your code for Problem 2c here
+    val empty : unit -> ('key,'value) dict
+    val lookup : ('key,'value) dict -> 'key -> 'value option
+    val member : ('key,'value) dict -> 'key -> bool
+    val insert : ('key,'value) dict -> 'key -> 'value -> ('key,'value) dict
+    val remove : ('key,'value) dict -> 'key -> ('key,'value) dict
   end
- *)
+
 
 (* TODO: Uncomment the module type in the line immediately following
    this comment after completing Problem 2c *) 
-module ListDict (* : DICT *) =
+module ListDict: DICT  =
   struct
     type ('key,'value) dict = ('key * 'value) list
 
@@ -255,28 +259,35 @@ module ListDict (* : DICT *) =
        is never more than one occurrence of a given key, and the list
        is always in sorted order. *)
               
-    let empty () = []
+    let empty (): ('key,'value) dict = []
 
-    let rec lookup d k =
+    let rec lookup (d:('key,'value) dict) (k:'key): 'value option =
       match d with
       | [] -> None
       | (hd_k,hd_v)::tl -> if hd_k=k then Some hd_v
                            else lookup tl k
 
-    let rec member d k =
+    let rec member (d:('key,'value) dict) (k:'key): bool =
       match d with
       | [] -> false
       | (hd_k,hd_v)::tl -> hd_k=k || member tl k
 
-    let rec insert d k v =
+    let rec insert (d:('key,'value) dict) (k:'key) (v:'value): ('key,'value) dict =
       match d with
       | [] -> [(k,v)]
       | (hd_k,hd_v)::tl -> if (k < hd_k || k = hd_k)
                            then (k,v)::(hd_k,hd_v)::tl
                            else (hd_k,hd_v)::insert tl k v
 
-    let rec remove d k = d  (* TODO: Add types for problem 2b *)
-      (* TODO: Insert your code for Problem 2a here *)
+    let rec remove (d:('key,'value) dict) (k:'key) = 
+        match d with
+            | [] -> []
+            | (hd_k,hd_v)::tl -> if (k = hd_k)
+                                    then remove tl k
+                                    else
+                                        if (k < hd_k)
+                                        then (hd_k,hd_v)::remove tl k
+                                            else d
   end
 
 (* Problem 2a: Replace the implementation of "remove" with a correct
@@ -313,18 +324,44 @@ module ListDict (* : DICT *) =
    below provide some initial code that you can build on.
    Note: This is a simple exercise, showing how easy it is to swap
    one implementation with another. *)
+   
+type prop = string
+
+type form' =
+  | True
+  | False
+  | Prop of prop 
+  | And of form' * form'
+  | Or of form' * form'
+  | Imp of form' * form'
+  | Not of form'
+
+type env = (prop * bool) list
 
 (* return the value of proposition p in environment e *)
 type env' = (string,bool) ListDict.dict
 let lookup' (e:env') (p:prop) : bool option =
   ListDict.lookup e p
+ 
+let lookup2' (x: bool option): bool =
+    match x with
+        | None -> false
+        | Some a -> a
 
 let env0 = ListDict.empty()
 let env1 = ListDict.insert env0 "p" true
 
-(*
-let eval_prop' ... = ...
- *)
+let rec eval_prop (f:form') (e:env') : bool =
+    match f with
+        | True -> true
+        | False -> false
+        | Prop a -> lookup2' (lookup' e a)
+        | And (a, b) -> (eval_prop a e) && (eval_prop b e)
+        | Or (a, b) -> (eval_prop a e) || (eval_prop b e)
+        | Imp (a, b) -> (not (eval_prop a e)) || (eval_prop b e)
+        | Not a -> not (eval_prop a e) 
+
+
 
 (* Problem 2e: Note that the above dictionary is a functional version.
    Design an imperative version of the DICT signature, but include
@@ -339,21 +376,34 @@ let eval_prop' ... = ...
 module type IMP_DICT =
   sig
     type ('key,'value) dict
-    (* TODO: Insert your code for Problem 2e here *)
+    val empty : unit -> ('key,'value) dict
+    val lookup : ('key,'value) dict -> 'key -> 'value option
+    val insert : ('key,'value) dict -> 'key -> 'value -> unit
   end
 
 (* Problem 2f: Implement a module with type IMP_DICT.  Hint: do so by
    making a copy of the contents of the ListDict module, removing the
    functions member and remove, and modifying the rest as needed.  *)
 
-(*
+
 module ImpListDict : IMP_DICT =
   struct
-    type ('key,'value) dict = TODO: fill in the type and the definitions
-                                    of empty, lookup, and insert
-  end
+    type ('key,'value) dict = ('key * 'value) list
+    let empty (): ('key,'value) dict = []
 
-Some code to help test your implementation:
+    let rec lookup (d:('key,'value) dict) (k:'key): 'value option =
+      match d with
+      | [] -> None
+      | (hd_k,hd_v)::tl -> if hd_k=k then Some hd_v
+                           else lookup tl k
+
+    let rec insert (d:('key,'value) dict) (k:'key) (v:'value): unit =
+      match d with
+      | [] -> d = [(k,v)]
+      | (hd_k,hd_v)::tl -> if (k < hd_k || k = hd_k)
+                           then d = (k,v)::(hd_k,hd_v)::tl
+                           else d = (hd_k,hd_v)::insert tl k v
+  end
 
 type env'' = (string,bool) ImpListDict.dict
 let lookup'' (e:env'') (p:prop) : bool option =
@@ -362,4 +412,4 @@ let lookup'' (e:env'') (p:prop) : bool option =
 let e'' = ImpListDict.empty()
 let _ = ImpListDict.insert e'' "p" true
 
- *)    
+  
